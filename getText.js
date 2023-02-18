bd2ac1df9730a37c0e31ae721dc6ea45f07606c5
d0422 <rlfehd2013@naver.com>
2023-02-19 03:46:47 +0900

    [FEAT] 형식에 맞춰서 노션 블록을 만들어주는 getText 함수 작성

---
+++ getText.js
@@ -0,0 +1,39 @@
+module.exports = function getText(question, answer) {
+  return [
+    {
+      object: 'block',
+      type: 'paragraph',
+      paragraph: {
+        text: [
+          {
+            type: 'text',
+            text: {
+              content: question,
+            },
+            annotations: {
+              bold: true,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      object: 'block',
+      type: 'paragraph',
+      paragraph: {
+        text: [
+          {
+            type: 'text',
+            text: {
+              content: String(answer),
+            },
+          },
+        ],
+      },
+    },
+  ];
+};
